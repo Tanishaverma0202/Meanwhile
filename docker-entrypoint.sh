478e83113bc9146dd0cd8ce8db769c82f936b1d1
Tanisha Verma <tanishaverma0202@gmail.com>
2026-09-05 17:40:27 +0530

    Deploy new

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -eu
 
 port="${PORT:-80}"
-sed "s/__PORT__/${port}/g" /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default
+sed "s/__PORT__/${port}/g" /etc/nginx/sites-available/default > /etc/nginx/conf.d/meanwhile.conf
+rm -f /etc/nginx/sites-enabled/default
 
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
